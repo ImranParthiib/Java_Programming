684dae947b63712ca4146f9de67935214af383fb
Imran Parthib <imranparthib13@gmail.com>
2024-02-09 15:50:42 +0600

    playing with java

--- Java In VS Code/Java Programming Spell/src/UsingArrayFib.java
+++ Java In VS Code/Java Programming Spell/src/UsingArrayFib.java
@@ -1,26 +1,62 @@
 import java.util.Scanner;
+
 public class UsingArrayFib {
     public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
-        int nthTerm = 
-        int[] A = new int[nthTerm];
+        Scanner scanner = new Scanner(System.in);
 
-        int firstNum = 0;
-        int secondNum = 1;
+        // Prompt the user to choose an option
+        System.out.println("Choose an option:");
+        System.out.println("1. Display Fibonacci sequence up to the nth term");
+        System.out.println("2. Display the value at a specific index in the Fibonacci sequence");
+        int option = scanner.nextInt();
 
-        System.out.print(firstNum + ","+ secondNum );
- 
-        int nextNum = firstNum + secondNum;
-        for (int i = 0; i < nthTerm; i++) {
-            A[i] = nextNum;
-            
-            firstNum = secondNum;
-            secondNum = nextNum;
-            nextNum = firstNum + secondNum;
+        switch (option) {
+            case 1:
+                // Prompt the user to enter the number of terms
+                System.out.print("Enter the number of terms: ");
+                int nthTerm = scanner.nextInt();
+                displayFibonacciSequence(nthTerm);
+                break;
+            case 2:
+                // Prompt the user to enter the index
+                System.out.print("Enter the index to display value from Fibonacci sequence: ");
+                int index = scanner.nextInt();
+                int value = getFibonacciValueAtIndex(index);
+                System.out.println("Value at index " + index + " in the Fibonacci sequence is: " + value);
+                break;
+            default:
+                System.out.println("Invalid option.");
         }
 
-        for (int i = 0; i < nthTerm; i++) {
-            System.out.print(","+A[i] );
+        scanner.close();
+    }
+
+    // Method to display Fibonacci sequence up to nth term
+    public static void displayFibonacciSequence(int nthTerm) {
+        int[] fibonacciSequence = new int[nthTerm];
+        fibonacciSequence[0] = 0;
+        fibonacciSequence[1] = 1;
+        System.out.print("Fibonacci Sequence: " + fibonacciSequence[0] + ", " + fibonacciSequence[1]);
+        for (int i = 2; i < nthTerm; i++) {
+            fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+            System.out.print(", " + fibonacciSequence[i]);
         }
+        System.out.println();
+    }
+
+    // Method to get the value at a specific index in the Fibonacci sequence
+    public static int getFibonacciValueAtIndex(int index) {
+        if (index <= 1) {
+            return index;
+        }
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 2; i <= index; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
     }
 }
